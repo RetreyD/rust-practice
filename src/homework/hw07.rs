e149b3bf5b784b217d88814e6842cdb538483b0e
RetreyD <ktoto25012@gmail.com>
2025-05-16 14:55:14 +0300

    Update hw07.rs

---
+++ src/homework/hw07.rs
@@ -0,0 +1,13 @@
+fn invert_the_case(s: String) -> String {
+    s.chars()
+        .map(|c| {
+            if c.is_uppercase() {
+                c.to_lowercase().collect::<String>()
+            } else if c.is_lowercase() {
+                c.to_uppercase().collect::<String>()
+            } else {
+                c.to_string()
+            }
+        })
+        .collect()
+}
